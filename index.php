c6f6f338b13b7ecbf0deeb93a99b5bb10845d9c8
ch3ll0v3k <ch3ll0v3k@yandex.com>
2019-08-21 20:00:09 +0200

    descr:[update]

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <?php
 
 $path = $_SERVER["SCRIPT_FILENAME"];
-file_put_contents("php://stdout", "\nRequested: $path");
+
+function console( $data ){
+  file_put_contents("php://stdout", "$data\n");
+}
 
 if (preg_match('/\.(?:png|jpg|jpeg|gif|js|css)$/', $_SERVER["REQUEST_URI"])) {
   // сервер возвращает файлы напрямую.
   return false;
 }
+
+// $indexFiles = ['index.html', 'index.php'];
+// $routes = [
+//   '^/api(/.*)?$' => '/index.php'
+// ];
+// $requestedAbsoluteFile = dirname(__FILE__) . $_SERVER['REQUEST_URI'];
+// foreach ($routes as $regex => $fn){
+//   if (preg_match('%'.$regex.'%', $_SERVER['REQUEST_URI'])){
+//     $requestedAbsoluteFilec = dirname(__FILE__) . $fn;
+//     break;
+//   }
+// }
+// console( $requestedAbsoluteFilec );
+
+
 
 echo "<p>Добро пожаловать в PHP</p>";
 
